99f814e9adbbf6eb95bfdbf5af2830b1c6d91761
Varun <warunhasija@gmail.com>
2016-08-20 13:09:13 +0530

    added tags and some changes in readme

---
+++ server/app-server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+// var config = require("./config");
+var path = require("path");
+
+app.use('/public', express.static(__dirname + '/../app/public'));
+app.use('/node_modules', express.static(__dirname + '/../node_modules'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve((__dirname + '/../app' + '/index.html')));
+});
+
+app.listen(3000, function() {
+	console.log("Listening on 3000 port.")
+});
