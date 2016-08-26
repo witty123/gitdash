cf720fcb6f9d7a8c0a658e2aa7a44d2ba23a297b
Varun <warunhasija@gmail.com>
2016-08-26 16:45:36 +0530

    codeclimate wip

--- app/components/pullrequest-list/service.js
+++ app/components/pullrequest-list/service.js
@@ -4,15 +4,15 @@ angular.
   module('pullrequestList').
   service('pullrequestSearch', ['$http', function($http){
   return ({
-  	getpullEvent: function(user, access_token){
-	  	return ($http.get("https://api.github.com/users/" + user + 
+    getpullEvent: function(user, access_token){
+      return ($http.get("https://api.github.com/users/" + user + 
        "/events?page=1&per_page=300&access_token=" + access_token));
-	  },
+    },
     confirmpullEvent: function(url, access_token){
       return ($http.get(url+ "?access_token=" + access_token));
     },
     getSearch: function () {
-	    return ($http.get("/api"));
-	  }
-	});
+      return ($http.get("/api"));
+    }
+  });
   }]);
